7766ff3f650cd63c811f9985ccc5107cbfa0265d
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-08-18 21:02:30 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+console.log("2">1);
+//before comparing you should always ensure the datatypes of the both should be the same
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+console.log(null<0);
+//in js equality and comparision are different both works in different ways
+console.log(undefined<0);
+console.log(undefined==0);
+console.log(undefined<=0);
+
+//=== ->triple equator it checks strictly it not only checks the values but also checks the datatype
+console.log("2"==2);
+console.log("2"===2);
+
