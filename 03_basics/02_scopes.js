020fdae86544b095483bb3d54b83e2919241a5c2
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-09-12 20:41:27 +0000

    scopeeee

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,17 +1,56 @@
 //var c=100 we should avoid using var
-let a=98
-if (true){
-    let a=10
-    const b=30
-    console.log("INNER:",a);
-}
-//when er use {} with if else or function then its scope
-for(let i=0;i<array.length;i++){
-    const element=array[i];
-}
+// let a=98
+// if (true){
+//     let a=10
+//     const b=30
+//     console.log("INNER:",a);
+// }
+// //when er use {} with if else or function then its scope
+// for(let i=0;i<array.length;i++){
+//     const element=array[i];
+// }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+//nested scope
+function one(){
+    const username="sheetal"
+
+    function two(){
+    const website="insta"
+    console.log(username);//it can access the values from first scope
+    }
+    // console.log(website);//this cannot 
+
+    two()
+}
+//one()
+
+if(true){
+    const username="sheetal"
+    if(username=="sheetal"){
+        const website="pinterest"
+        // console.log(username+website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+
+//***********interesting ***********//
+//two syntax of declaring functionand how they are different
+
+
+addone(5)//this will not throw the error
+function addone(num){
+    return num+1
+}
+
+addTwo(5)//this will throw the error
+const addTwo=function(num){
+    return num2+2
+}
 
 
