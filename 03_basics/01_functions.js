1c40a7888bb96690335ecf7a29d52207f1cb1d14
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-09-12 19:48:44 +0000

    function

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,34 @@
+//function mein when you write a big code and you amke a package of it and use it anytime with writing it again and again
+function sayHeyy(){
+console.log("H");
+console.log("E");
+console.log("Y");
+console.log("Y");
+}
+
+//sayHeyy() that how you refer the function
+
+// function addTwo(number1,number2){
+//     console.log(number1+number2);
+// }
+// const result=addTwo(3,7);
+// console.log("Result: ",result)//the value is not stored in result when we run that we still get result as undefined
+
+
+//here we store the value of result(correct method)
+function addTwo(number1,number2){
+    let result=number1+number2;
+    return result;
+    console.log("hjjk")//it will never get printed because after you return anything in function nothing will get executed
+}
+const result=addTwo(4,6)
+console.log("Result: ",result)
+
+function loginUserMessage(username="sam"){
+    if(username==undefined){
+         console.log("Please enter username");
+    }
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("sheetal"))
+// console.log(loginUserMessage())//when you dont pass any value then the reult will be undefined just logged in 
