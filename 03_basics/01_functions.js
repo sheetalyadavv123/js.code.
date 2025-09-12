cfd3b13d2a1f6082b05b325fc8e1fb68be95fa12
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-09-12 20:04:21 +0000

    functionssss

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -30,5 +30,33 @@ function loginUserMessage(username="sam"){
     }
     return `${username} just logged in`
 }
-console.log(loginUserMessage("sheetal"))
+// console.log(loginUserMessage("sheetal"))
 // console.log(loginUserMessage())//when you dont pass any value then the reult will be undefined just logged in 
+
+
+//when we dont know how many arguments (in shopping cart)
+function calculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+//console.log(calculateCartPrice(200,400,500))//...is spread operatorn as well as rest operator
+
+const user={
+    username:"sheetal",
+    price:9999
+}
+function handleObject(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+
+//handleObject(user)
+handleObject({
+    username:"sam",
+    price:499
+})
+
+const myNewArray=[200,400,900]
+function returnSecondValue(getArray){
+     return getArray[2]
+}
+
+console.log(returnSecondValue(myNewArray));
