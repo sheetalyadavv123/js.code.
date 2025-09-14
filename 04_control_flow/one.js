a36b6f918b1bf4f6053c6b899ef4aa5e74cfbc86
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-09-14 20:20:47 +0000

    if

---
+++ 04_control_flow/one.js
@@ -0,0 +1,44 @@
+//if
+// const isUserLoggedIn=true
+// const temp=41
+// if(temp<50){
+//     console.log("less than 50");
+// }else{
+//     console.log("temperature is greater than 50");
+// }
+// console.log("executed");
+
+// const score=200
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power: ${power}`);
+// }
+// console.log(`user power: ${power}`);
+
+const balance=1000
+// if(balance>500) console.log("test"),console.log("test2"); //its not a good practice to write code like this
+
+// if(balance<500){
+//    console.log("less than");
+// }else if(balance<750){
+//     console.log("less than 500");
+// }else{
+//     console.log("less than 1200");
+// }
+
+const userLoggedIn=true;
+const debitCard=true;
+const loggedInFromGoogle=false;
+const loggedInFromEmail=true;
+if(userLoggedIn && debitCard && 2==2){
+    console.log("allowed to buy");
+}
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("user logged in");
+}
+
+
+
+ 
+
+
