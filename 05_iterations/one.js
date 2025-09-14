28dfadd647dfe49a1aa3711f92191e2f499044e3
sheetalyadavv123 <sheetalyad7@gmail.com>
2025-09-14 22:06:21 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,50 @@
+//for
+for(let i=0;i<10;i++){
+    const element=i;
+    if(element == 5){
+        // console.log("5 is best")
+    }
+    console.log(element);
+}
+
+for(let i=0;i<=10;i++){
+   //console.log(`outer loop value: ${i}`);
+   for(let j=0;j<=10;j++){
+    //console.log(`inner loop value: ${j} and ouet loop ${i}`);
+    //console.log(i + '*' + j + '=' + i*j );
+    
+   }
+   
+}
+
+// let myArray=["flash","batman","superman"]
+
+// for(let i=0;i<myArray.length;i++){
+//     const element=myArray[i];
+//     console.log(element);
+    
+// }
+
+// break and continue
+// for(let index=1;index<=20;index++){
+//     if(index==5){
+//         console.log(`detected 5`);
+//         break;
+//     }
+//     console.log(`value of i is ${index}`);
+    
+// }
+
+for(let index=1;index<=20;index++){
+    if(index==5){
+        console.log(`detected 5`);
+        continue;
+    }
+    console.log(`value of i is ${index}`);
+    
+}
+
+
+
+
+
